beb66ce9518288748c9e1693bb1458f290078847
Domaw1 <nimur.lalov@bk.ru>
2023-11-19 12:51:48 +0500

    add logView

---
+++ src/views/LogView.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="main">
+    <div class="form">
+      <h1>Введите ваш логин</h1>
+      <v-responsive class="mx-auto" max-height="100px">
+        <v-text-field
+          label="Логин"
+          hide-details="auto"
+          style="width: 300px;"
+        ></v-text-field>
+      </v-responsive>
+      <button>Войти</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { VTextField, VResponsive } from "vuetify/lib/components/index.mjs";
+
+export default {
+  name: "HomeView",
+  components: {
+    VTextField,
+    VResponsive,
+  },
+};
+</script>
+
+<style scoped>
+.main {
+  height: 100%;
+  background-color: #ea526f;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+  background-color: aliceblue;
+  height: 370px;
+  width: 400px;
+  border-radius: 10px;
+  box-shadow: 10px 10px 10px black;
+}
+
+button {
+  height: 40px;
+  width: 200px;
+  background-color: #ea526f;
+  border-radius: 5px;
+  height: 50px;
+  color: aliceblue;
+  box-shadow: 1px 7px 7px black;
+}
+
+h1 {
+  color: grey;
+  margin-bottom: 50px;
+}
+</style>
