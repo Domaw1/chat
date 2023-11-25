c9831dd3724a0bd9eac49c3d4cfeb573ffcc69f4
Domaw1 <nimur.lalov@bk.ru>
2023-11-25 17:30:03 +0500

    add userProfile

---
+++ src/views/UserProfile.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    {{ currentUser }}
+  </div>
+</template>
+
+<script>
+import { useRoute } from 'vue-router';
+import { getCurrentUser } from '@/db/db';
+
+export default {
+    name: 'UserProfile',
+
+    setup() {
+        const route = useRoute();
+        const currentUser = route.params.email;
+        console.log(getCurrentUser());
+        return {
+            currentUser,
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
