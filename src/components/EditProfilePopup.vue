e22a3270437dab01470146d5e105a6503fda0248
Domaw1 <nimur.lalov@bk.ru>
2023-11-27 16:32:58 +0500

    add edit popup

---
+++ src/components/EditProfilePopup.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="popup" v-if="isOpen" @click.stop="hideDialog">
+    <div class="popup-content" @click.stop>
+      <input type="text" placeholder="Изменить имя..." v-model="propertiesToUpdate.name"/>
+      <input type="text" placeholder="Изменить почту..." v-model="propertiesToUpdate.email"/>
+      <my-button @click="changeProfile">Изменить</my-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import MyButton from "./UI/MyButton.vue";
+import { ref } from "vue";
+
+export default {
+  name: "EditProfilePopup",
+  components: { MyButton },
+  props: {
+    isOpen: {
+      type: Boolean,
+    },
+  },
+
+  emits: ["hideDialog", "changeProfile"],
+
+  setup(props, { emit }) {
+    const propertiesToUpdate = ref({
+      name: "",
+      email: "",
+    });
+
+    const hideDialog = () => {
+      emit("hideDialog");
+    };
+
+    const changeProfile = () => {
+      emit("changeProfile", propertiesToUpdate);
+    };
+
+    return {
+      hideDialog,
+      changeProfile,
+      propertiesToUpdate,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.popup {
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  display: flex;
+}
+
+.popup-content {
+  margin: auto;
+  background: aliceblue;
+  align-items: center;
+  border-radius: 12px;
+  min-height: 320px;
+  min-width: 500px;
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+}
+
+input {
+  width: 400px;
+  border: 1px solid #ea526f;
+  padding: 5px;
+  border-radius: 5px;
+}
+</style>
