55762bf5f9fa492a7bdecfdb280304d6277b672a
Domaw1 <nimur.lalov@bk.ru>
2023-11-27 16:32:34 +0500

    add ui

---
+++ src/components/UI/MyButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "MyButton",
+};
+</script>
+
+<style scoped>
+button {
+  width: 250px;
+  height: 35px;
+  font-size: 20px;
+  box-shadow: 1px 1px 6px black;
+  border: none;
+  border-radius: 7px;
+  background-color: #ea526f;
+  color: aliceblue;
+}
+</style>
