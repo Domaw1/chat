28fc7b6cd0a8bfa8b489e5f7b10b128c7d6960ff
Domaw1 <nimur.lalov@bk.ru>
2023-11-29 18:22:36 +0500

    aaa

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,8 @@ module.exports = defineConfig({
      // НЕ РАБОТАЕТ
   },
 
+  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
